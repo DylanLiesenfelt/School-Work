60d1f3328aa08cb2e7dce7bd3646dadd955f5bf6
DylanLiesenfelt <dylanjames.code@gmail.com>
2024-06-28 15:27:40 -0400

    HW 4

--- CDA3203/Assignment 3/half adder/design.vhd
+++ CDA3203/Assignment 3/half adder/design.vhd
@@ -4,10 +4,8 @@ use IEEE.std_logic_1164.all;
 -- ENTITY
 entity HalfAdder is
 port(
-	a: in std_logic;
-    b: in std_logic;
-    sum: out std_logic;
-    carry: out std_logic);
+	a, b: in std_logic;
+    sum, carry: out std_logic);
 end HalfAdder;
 
 -- ARCHITECTURE
